c19a3344931fa25b51dcdfcfabee7327b311cffa
BrianHammond <brianearlhammond@gmail.com>
2025-01-28 13:49:00 +0900

    code changes

--- main.py
+++ main.py
@@ -14,42 +14,44 @@ except ModuleNotFoundError: # if it's not then it will automatically be installe
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6 import uic
+import os
 from api import get_pokemon_info
 
 class UI(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
-
+        
         self.pokemon_name.returnPressed.connect(self.pokemon_get)
 
     def pokemon_get(self):
-       
-       pokemon_name = self.pokemon_name.text()
 
-       get_pokemon_info(pokemon_name)
+        os.system("CLS")
+              
+        pokemon_name = self.pokemon_name.text()
 
-       print(f"Name: {get_pokemon_info(pokemon_name)["name"]}")
-       print(f"Weight: {get_pokemon_info(pokemon_name)["weight"]}")
+        get_pokemon_info(pokemon_name)
+                     
+        print(f"Name: {get_pokemon_info(pokemon_name)["name"]}")
+        print(f"Weight: {get_pokemon_info(pokemon_name)["weight"]}")
 
-       for x in range (0,500): # this is used to loop through all the available abilities, i set it to 500 times and will cycle through as much and break out of it
-           try:
-               get_pokemon_info(pokemon_name)["abilities"][x]["ability"]["name"]
-               print(f"Ability {x+1}: {get_pokemon_info(pokemon_name)["abilities"][x]["ability"]["name"]}")
-               x +=1
-           except IndexError:
-               break
+        for x in range (0,500): # this is used to loop through all the available abilities, i set it to 500 times and will cycle through as much and break out of it
+            try:
+                get_pokemon_info(pokemon_name)["abilities"][x]["ability"]["name"]
+                print(f"Ability {x+1}: {get_pokemon_info(pokemon_name)["abilities"][x]["ability"]["name"]}")
+                x +=1
+            except IndexError:
+                break
 
-       for x in range (0,500): # this is used to loop through all the available moves, i set it to 500 times and will cycle through as much and break out of it
-           try:
-               get_pokemon_info(pokemon_name)["moves"][x]["move"]["name"]
-               print(f"Move {x+1}: {get_pokemon_info(pokemon_name)["moves"][x]["move"]["name"]}")
-               x +=1
-           except IndexError:
-               break
-       print ("thank you")
+        for x in range (0,500): # this is used to loop through all the available moves, i set it to 500 times and will cycle through as much and break out of it
+            try:
+                get_pokemon_info(pokemon_name)["moves"][x]["move"]["name"]
+                print(f"Move {x+1}: {get_pokemon_info(pokemon_name)["moves"][x]["move"]["name"]}")
+                x +=1
+            except IndexError:
+                break
+        print ("thank you")
 
-# Show/Run app
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     UIWindow = UI()
