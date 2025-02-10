63c40207bf4ed3df47e2af5ff0da3ea3cdd8fac9
BrianHammond <brianearlhammond@gmail.com>
2025-02-10 09:22:50 +0900

    added save load

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import qdarkstyle
 from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PySide6.QtCore import QSettings
 from rest_url import api_request
 from main_ui import Ui_MainWindow as main_ui
 from about_window import AboutWindow
@@ -10,12 +11,14 @@ class MainWindow(QMainWindow, main_ui):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.settings = QSettings('settings.ini', QSettings.IniFormat)
+        self.loadSettings()
 
         #button
-        self.pokemon_name.returnPressed.connect(self.pokemon_get)
+        self.le_pokemon_name.returnPressed.connect(self.pokemon_get)
 
         #menubar
-        self.actionDark_Mode.toggled.connect(self.dark_mode)
+        self.actionDarkMode.toggled.connect(self.dark_mode)
         self.actionAbout.triggered.connect(self.show_about)
         self.actionAbout_Qt.triggered.connect(self.about_qt)
 
@@ -26,23 +29,23 @@ class MainWindow(QMainWindow, main_ui):
 
         os.system("CLS")
 
-        pokemon_info = api_request(self.pokemon_name.text())
+        pokemon_name = api_request(self.le_pokemon_name.text())
 
-        name = pokemon_info["name"]
+        name = pokemon_name["name"]
 
         self.pokemon_label.setText(name)
 
-        print(f"Name: {pokemon_info["name"]}")
-        print(f"Weight: {pokemon_info["weight"]}")
+        print(f"Name: {pokemon_name["name"]}")
+        print(f"Weight: {pokemon_name["weight"]}")
 
         for row in range (0,500): # this is used to loop through all the available moves, i set it to 500 times and will cycle through as much and break out of it
             try:
                 self.table1.setColumnCount(1)
                 self.table1.setHorizontalHeaderLabels(['Moves'])
                 self.table1.insertRow(row)
-                moves = pokemon_info["moves"][row]["move"]["name"]
+                moves = pokemon_name["moves"][row]["move"]["name"]
                 self.table1.setItem(row, 0, QTableWidgetItem(moves))
-                print(f"Move {row+1}: {pokemon_info["moves"][row]["move"]["name"]}")
+                print(f"Move {row+1}: {pokemon_name["moves"][row]["move"]["name"]}")
                 row +=1
             except IndexError:
                 break
@@ -52,19 +55,19 @@ class MainWindow(QMainWindow, main_ui):
                 self.table2.setColumnCount(1)
                 self.table2.setHorizontalHeaderLabels(['Abilities'])
                 self.table2.insertRow(row)
-                ability = pokemon_info["abilities"][row]["ability"]["name"]
+                ability = pokemon_name["abilities"][row]["ability"]["name"]
                 self.table2.setItem(row, 0, QTableWidgetItem(ability))
                 print(f"Ability {row+1}: {ability}")
                 row += 1
             except IndexError:
                 break
             
-            weight = pokemon_info["weight"]
+            weight = pokemon_name["weight"]
             self.weight_label.setText(str(weight))
 
         print ("thank you")
         
-        self.pokemon_name.clear()
+        self.le_pokemon_name.clear()
 
     def dark_mode(self, checked):
         if checked:
@@ -78,6 +81,24 @@ class MainWindow(QMainWindow, main_ui):
     def show_about(self):
         self.about_window = AboutWindow()
         self.about_window.show()
+
+    def closeEvent(self, event): #settings will save when closing the app
+        self.settings.setValue('window_size', self.size())
+        self.settings.setValue('window_pos', self.pos())
+        self.settings.setValue('dark_mode', self.actionDarkMode.isChecked())
+        event.accept()
+
+    def loadSettings(self): #settings will load when opening the app
+        size = self.settings.value('window_size', None)
+        pos = self.settings.value('window_pos', None)
+        dark = self.settings.value('dark_mode')
+        if size is not None:
+            self.resize(size)
+        if pos is not None:
+            self.move(pos)
+        if dark == 'true':
+            self.actionDarkMode.setChecked(True)
+            self.setStyleSheet(qdarkstyle.load_stylesheet_pyside6())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv) # needs to run first
